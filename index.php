045ba590a7c07931647c52ba51f47aa33fe02a49
razmiO <thomas.cuesta31@gmail.com>
2024-11-28 17:25:48 +0100

    Refactor print statements in index.php

--- index.php
+++ index.php
@@ -5,11 +5,11 @@ try {
     define("BASE_DIR", __DIR__);
     require_once BASE_DIR . '/utils.php';
 
-    print_r("[INDEX] : start" . PHP_EOL);
+    print_that("[INDEX]",  "start");
     $time1 = mark();
     require_once BASE_DIR . '/src/router/starter.php';
-    print_r(PHP_EOL . "[INDEX] : end" . PHP_EOL);
-    print_r("[INDEX] : duration " . measure($time1) . " ms" . PHP_EOL);
+    print_that("[INDEX]", "end");
+    print_that("[INDEX]", "duration " . measure($time1) . " ms");
 } catch (Throwable $th) {
     throw $th;
 }
@@ -21,13 +21,13 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script type="module" src="/public/index.js"></script>
+    <script type="module" src="/index.js"></script>
     <title>Document</title>
 </head>
 
 <body>
     <h1>Index</h1>
-    <img src="/public/assets/alien_image.png" alt="image">
+    <img src="/assets/alien_image.png" alt="image">
 </body>
 
 </html>
