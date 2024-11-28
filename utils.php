42aecd40d9dcc303ba54148265f0ff3bf1029947
razmiO <thomas.cuesta31@gmail.com>
2024-11-28 17:25:43 +0100

    Add print_that function to utils.php

--- utils.php
+++ utils.php
@@ -8,3 +8,8 @@ function measure(float $pastStamp): float
 {
     return (mark() - $pastStamp) * 1000;
 }
+
+function print_that(string $location, string $message)
+{
+    // print_r("[$location] : $message" . PHP_EOL);
+}
