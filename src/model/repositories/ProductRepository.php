cbd902c7ac50d35515125e99f99c2b30cacdfa66
razmiO <thomas.cuesta31@gmail.com>
2024-12-02 22:25:39 +0100

    Refactor ProductRepository to move it to the correct namespace

--- src/model/repositories/ProductRepository.php
+++ src/model/repositories/ProductRepository.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Model;
+namespace App\Model\Repository;
 
 use App\Lib\Repository;
 use App\Model\Entity\Product;
 use App\Lib\HTTP\Error;
+use Closure;
 use Exception;
 
 /**
@@ -14,20 +15,17 @@ class ProductRepository extends Repository
 {
 
     /**
-     * @return Product[] | void
+     * @return Product[]
      */
     public function findAll()
     {
+        /** @var Product[] */
+        $products = [];
         try {
             $query = "SELECT * FROM T_PRODUIT ORDER BY date_creation DESC LIMIT 10";
-            if (!$this->pdo) {
-                Error::HTTP500("Internal server error");
-                return;
-            };
             $prepared = $this->pdo->prepare($query);
             if (!$prepared || !$prepared->execute()) {
                 Error::HTTP500("Internal server error");
-                return;
             };
             /**
              * @var ProductType[] | false $products_from_db
@@ -35,45 +33,45 @@ class ProductRepository extends Repository
             $products_from_db = $prepared->fetchAll();
             if ($products_from_db === false || count($products_from_db) === 0) {
                 Error::HTTP404("No products found");
-                return;
             };
-            return array_map(fn($product) => Product::make($product), $products_from_db);
+            // @phpstan-ignore argument.type
+            $products = Product::makeBulk($products_from_db);
         } catch (Exception $e) {
             throw $e;
         } finally {
             $this->connection->close();
         }
+        return $products;
     }
 
+
+
     /**
-     * @return Product | void
+     * @return Product | null
      */
     public function findOne(string $id)
     {
+        $product = null;
         try {
             $query = "SELECT * FROM T_PRODUIT WHERE id = :id";
-            if (!$this->pdo) {
-                Error::HTTP500("Internal server error");
-                return;
-            }
             $prepared = $this->pdo->prepare($query);
             if (!$prepared || !$prepared->execute(["id" => $id])) {
                 Error::HTTP500("Internal server error");
-                return;
             }
             /**
              * @var ProductType | false $product_from_db
              */
             $product_from_db = $prepared->fetch();
-            if (!$product_from_db) {
+            if ($product_from_db === false) {
                 Error::HTTP404("Product not found");
-                return;
             }
-            return Product::make($product_from_db);
+            // @phpstan-ignore argument.type
+            $product = Product::make($product_from_db);
         } catch (Exception $e) {
             throw $e;
         } finally {
             $this->connection->close();
         }
+        return $product;
     }
 }
