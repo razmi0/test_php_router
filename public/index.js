5cef4d5a1193870a6c25e1c31437090577ec0796
razmiO <thomas.cuesta31@gmail.com>
2024-12-07 13:26:57 +0100

    Refactor index.js to add asset linked indicators

--- public/index.js
+++ public/index.js
@@ -1,3 +1,15 @@
 console.log("Hello from index.js");
+
+// js asset linked indicator
 const root = document.querySelector("#js-from-file");
-root.innerHTML = "<p>Hello from index.js</p>";
+root.innerHTML += "<p>JS Asset linked</p>";
+
+// css asset linked indicator
+const testAssetLinked = document.querySelector("#test-asset-linked");
+testAssetLinked.innerHTML = "<p>Asset linked</p>";
+
+// img asset linked indicator
+const testImgLinked = document.querySelector("#test-img-linked");
+if (!testImgLinked.complete || (typeof testImgLinked.naturalWidth != "undefined" && testImgLinked.naturalWidth == 0)) {
+  testImgLinked.remove();
+}
