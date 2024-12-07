31a581739888525bfe516eb8e28ccee7b88dc47b
razmiO <thomas.cuesta31@gmail.com>
2024-12-07 13:27:12 +0100

    Refactor Controller constructor to accept Request and Response objects

--- src/lib/Controller.php
+++ src/lib/Controller.php
@@ -3,10 +3,12 @@
 namespace App\Lib;
 
 use App\Lib\Interfaces\IController;
+use App\Lib\HTTP\Request;
+use App\Lib\HTTP\Response;
 
 abstract class Controller implements IController
 {
-    public function __construct() {}
+    public function __construct(protected Request $request, protected Response $response) {}
 
     public static function js(string $js): string
     {
